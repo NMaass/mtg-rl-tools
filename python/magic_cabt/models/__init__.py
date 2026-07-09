4fec2d532b2e54dbcb7f737d381958abff9c9f8c
Nick Maassen <Nicholasmaassen@yahoo.com>
2026-07-09 13:48:22 -0500

    Add model config package exports

---
+++ python/magic_cabt/models/__init__.py
@@ -0,0 +1,5 @@
+"""Model configuration helpers for MTG CABT policies."""
+
+from .configs import MODEL_CONFIGS, estimate_parameter_count, get_model_config
+
+__all__ = ["MODEL_CONFIGS", "estimate_parameter_count", "get_model_config"]
